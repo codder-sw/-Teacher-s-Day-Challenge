e9219516f682a3e94582cf76dbb95ec1e01235a8
SHIVAM WAGHULE <shivamwaghule2005@gmail.com>
2025-08-01 22:58:12 +0530

    Create  71A. Way Too Long Words.java

---
+++ Day-08/71A. Way Too Long Words.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.nextLine(); // To consume the newline after reading n
+        
+        for (int i = 0; i < n; ++i) {
+            String word = scanner.nextLine();
+            
+            if (word.length() > 10) {
+                System.out.println(word.charAt(0) + 
+                                  String.valueOf(word.length() - 2) + 
+                                  word.charAt(word.length() - 1));
+            } else {
+                System.out.println(word);
+            }
+        }
+        
+        scanner.close();
+    }
+}
