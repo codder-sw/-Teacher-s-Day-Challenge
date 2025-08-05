c4e533144d9641abdf1b9cda27f980f118ab5fb2
SHIVAM WAGHULE <shivamwaghule2005@gmail.com>
2025-08-05 23:55:44 +0530

    Create 112A-Petya and Strings.java

---
+++ Day-12/112A-Petya and Strings.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PetyaAndStrings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s1 = scanner.nextLine();
+        String s2 = scanner.nextLine();
+        
+        s1 = s1.toLowerCase();
+        s2 = s2.toLowerCase();
+        
+        int result = s1.compareTo(s2);
+        
+        if (result < 0) {
+            System.out.println("-1");
+        } else if (result > 0) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    }
+}
