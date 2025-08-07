a34913134d66b84ba0109a47482476bb33213202
SHIVAM WAGHULE <shivamwaghule2005@gmail.com>
2025-08-07 22:52:11 +0530

    Create 281A-Word Capitalization.cpp

---
+++ Day14/281A-Word Capitalization.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+using namespace std;
+
+int main() {
+    string word;
+    cin >> word;
+    if (!word.empty()) {
+        word[0] = toupper(word[0]);
+    }
+    cout << word << endl;
+    return 0;
+}
