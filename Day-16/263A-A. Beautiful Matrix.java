1f1fcf8ad0f3efc1d311f3b43085d262eb481c02
SHIVAM WAGHULE <shivamwaghule2005@gmail.com>
2025-08-09 21:46:54 +0530

    Create 263A-A. Beautiful Matrix.java

---
+++ Day-16/263A-A. Beautiful Matrix.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BeautifulMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] matrix = new int[5][5];
+        int row = 0, col = 0;
+
+        // Read the matrix and find the position of '1'
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                matrix[i][j] = scanner.nextInt();
+                if (matrix[i][j] == 1) {
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+
+        // Calculate the minimum number of moves
+        int moves = Math.abs(row - 2) + Math.abs(col - 2);
+        System.out.println(moves);
+    }
+}
