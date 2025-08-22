1c3c5cc23de63e48c60c2ba4b692d54d784f202e
SHIVAM WAGHULE <shivamwaghule2005@gmail.com>
2025-08-23 00:07:16 +0530

    Create 266B-Queue at the School.cpp

---
+++ Day 22/266B-Queue at the School.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n, t;
+    string s;
+    cin >> n >> t >> s;
+    while (t--) {
+        for (int i = 0; i < n - 1; i++) {
+            if (s[i] == 'B' && s[i+1] == 'G') {
+                swap(s[i], s[i+1]);
+                i++; // Skip the next position as it is already swapped
+            }
+        }
+    }
+    cout << s << endl;
+    return 0;
+}
