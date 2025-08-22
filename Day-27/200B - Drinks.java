55fc68a2ed7bf0c89759c641b6e281ac628ba517
SHIVAM WAGHULE <shivamwaghule2005@gmail.com>
2025-08-23 00:53:47 +0530

    Create 200B - Drinks.java

---
+++ Day-27/200B - Drinks.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            int p = sc.nextInt();
+            sum += p;
+        }
+        double result = sum / n;
+        System.out.printf("%.12f\n", result);
+    }
+}
